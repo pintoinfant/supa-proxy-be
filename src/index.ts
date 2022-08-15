bf092d7d345774e7a7a222c9cc11e5429bfcc728
Pinto565 <pintoinfant5650@gmail.com>
2022-08-15 08:26:58 +0530

    make it better

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { redisClient } from "./helpers/client";
 
 redisClient.connect();
 
-server.listen(process.env.API_PORT || "5000", () => {
+server.listen(process.env.API_PORT || 5000, () => {
   console.log(
     `The API server has successfully started. \nListening at ${
       process.env.APP_BASE_URL || "http://localhost:5000"
